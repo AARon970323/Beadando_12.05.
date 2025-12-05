a5b76c6ef3a2ab4f607fe0b4f8e5300a4594cc21
Hegyi_Aron <hegyi.aron12@gmail.com>
2025-12-05 16:34:02 +0100

    feature/failing-test.
    Updated the expected result for the add function test to 4.

--- tests/unit/test_calculator_unit.py
+++ tests/unit/test_calculator_unit.py
@@ -3,7 +3,7 @@ from calculator.calculator import Calculator
 
 def test_add():
     c = Calculator()
-    assert c.add(1, 2) == 3
+    assert c.add(1, 2) == 4  #szándákosan rossz
 
 def test_sub():
     c = Calculator()
